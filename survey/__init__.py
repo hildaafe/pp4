f0fd393dded89b2361fc1704800658498d75602e
hildaafe <86850310+hildaafe@users.noreply.github.com>
2021-07-08 22:52:46 +0200

    Update __init__.py

--- survey/__init__.py
+++ survey/__init__.py
@@ -55,12 +55,13 @@ class Player(BasePlayer):
 
 # PAGES
 
-class CognitiveReflectionTest(Page): form_model = 'player'
-       form_fields = ['crt_bat', 'crt_widget', 'crt_lake']
+class CognitiveReflectionTest(Page): 
+    form_model = 'player'
+    form_fields = ['crt_bat', 'crt_widget', 'crt_lake']
     
 class Demographics(Page):
-       form_model = 'player'
-       form_fields = ['age', 'gender']
+    form_model = 'player'
+    form_fields = ['age', 'gender']
 
 
 
