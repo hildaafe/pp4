bcf476ca14a67098ca442f165a3fb3088db41c62
hildaafe <86850310+hildaafe@users.noreply.github.com>
2021-07-08 23:06:48 +0200

    Update __init__.py

--- survey/__init__.py
+++ survey/__init__.py
@@ -1,9 +1,9 @@
 from otree.api import Currency as c, currency_range
-from ._builtin import Page, WaitPage
 from . import models
 from otree.api import (
     models,
     widgets,
+    Page,
     BaseConstants,
     BaseSubsession,
     BaseGroup,
